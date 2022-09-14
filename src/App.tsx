9ec2924b1c1fbc23843731afca9197ba7a56fa7c
Gustavo Seabra <devguga87@gmail.com>
2022-09-13 23:22:28 -0300

    adiciona estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {ThemeProvider} from 'styled-components'
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
      <h1>Hello</h1>
+     <GlobalStyle/>
     </ThemeProvider>
   )
 }
