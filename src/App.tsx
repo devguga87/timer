ef39eecbb3298a39502f99b1894c7da0f33e5676
gustavo.seabra <devguga87@gmail.com>
2022-09-15 17:11:28 -0300

    s

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import {ThemeProvider} from 'styled-components'
+import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 const App = () => {
   return (
     <ThemeProvider theme={defaultTheme}>
-     <h1>Hello</h1>
-     <GlobalStyle/>
+      <h1>Hello</h1>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
